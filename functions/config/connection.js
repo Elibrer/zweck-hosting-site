853ba2f59476cb3eece15a1c5ba15009120d2604
Elibrer <eli.brer@gmail.com>
2024-04-23 17:42:38 +0200

    Updates

--- functions/config/connection.js
+++ functions/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Zweck-hosting', {
+mongoose.connect(process.env.MONGODB_URI || process.env.mongodb_uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
