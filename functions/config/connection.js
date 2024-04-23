a96f2133a8df568960771266dba7a84ced097eb6
Elibrer <eli.brer@gmail.com>
2024-04-23 18:05:59 +0200

    Updates

--- functions/config/connection.js
+++ functions/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionURI = process.env.MONGODB_URI || process.env.FUNCTIONS_MONGODB_URI;
+const connectionURI = "mongodb+srv://elibrer:elibrer@zweck-hosting.sf1ueo9.mongodb.net/?retryWrites=true&w=majority&appName=Zweck-hosting"
 mongoose.connect(connectionURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
